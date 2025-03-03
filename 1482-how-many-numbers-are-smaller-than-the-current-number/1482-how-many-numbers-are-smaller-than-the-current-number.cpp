3432346b60eb50c034f76ebdc05028c5d412d1e7
Anshika Yadav <146202301+AnshikaYadav8@users.noreply.github.com>
2025-03-03 23:17:38 +0530

    Time: 3 ms (66.61%), Space: 14.9 MB (6.9%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -4,13 +4,11 @@ public:
         vector<int> sortedNums = nums;
         sort(sortedNums.begin(), sortedNums.end());
         unordered_map<int,int> mp;
-
         for(int i = 0; i < sortedNums.size(); i++) {
             if(mp.find(sortedNums[i]) == mp.end()) {  
                 mp[sortedNums[i]] = i;  
             }
         }
-
         vector<int> result;
         for(auto x: nums){
             result.push_back(mp[x]);
